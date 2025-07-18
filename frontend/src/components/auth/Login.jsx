0e49e65b63199c6caa4ac3cb882fe8c295515fe5
Vaibhav Singh <117469776+vaibhavsingh26@users.noreply.github.com>
2025-07-18 20:35:08 +0530

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -66,7 +66,7 @@ const Login = () => {
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="enter you email"
                         />
                     </div>
 
@@ -77,7 +77,7 @@ const Login = () => {
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="enter you password"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
