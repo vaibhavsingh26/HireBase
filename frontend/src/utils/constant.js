ad50c9ce3c023c35fbcac34c6c6186f28ec88bf4
Vaibhav Singh <117469776+vaibhavsingh26@users.noreply.github.com>
2025-07-18 20:32:36 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://hirebase.vercel.app/api/v1/user";
-export const JOB_API_END_POINT = "https://hirebase.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://hirebase.vercel.app/api/v1/applications";
-export const COMPANY_API_END_POINT = "https://hirebase.vercel.app/api/v1/company";
+export const USER_API_END_POINT = "https://hirebase1.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://hirebase1.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://hirebase1.onrender.com/api/v1/applications";
+export const COMPANY_API_END_POINT = "https://hirebase1.onrender.com/api/v1/company";
